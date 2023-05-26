a6b851bf89f3cc865e1c0336a557edac9b0e3abe
Jose luis <boxixz0991@gmail.com>
2023-05-26 14:28:28 +0200

    fixing api_key

--- src/services/dataApi.js
+++ src/services/dataApi.js
@@ -2,7 +2,9 @@
 // const q = 'vegeta';
 
 export const getGifs = q => {
-	const url = `https://api.giphy.com/v1/gifs/search?api_key=ZJvcXf8B7tIT7cJtSOj5QIBZoGtaq0XI&q=${q}&limit=10&offset=0&rating=g&lang=en`;
+	const url = `https://api.giphy.com/v1/gifs/search?api_key=${
+		import.meta.env.VITE_API_KEY
+	}=${q}&limit=10&offset=0&rating=g&lang=en`;
 
 	return fetch(url)
 		.then(resultado => resultado.json())
